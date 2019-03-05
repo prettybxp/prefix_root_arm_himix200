76eff8bcf2264f1ebbe59d633a138997e90056f4
Carl <ifbao@qq.com>
2019-03-05 15:15:20 +0800

    add ssh

--- environment-setup.sh
+++ environment-setup.sh
@@ -1,9 +1,13 @@
 
 
+export "CPPFLAGS=-I/home/ry10091/prefix_root_arm_himix200/include"
+export "LDFLAGS=-L/home/ry10091/prefix_root_arm_himix200/lib"
+
+
 export "CC=arm-himix200-linux-gcc"
 export "CXX=arm-himix200-linux-g++"
+export "STRIP=arm-himix200-linux-strip"
 export "GCC_HOME=/opt/hisi-linux/x86-arm/arm-himix200-linux/"
 export "CPLUS_INCLUDE_PATH=/opt/hisi-linux/x86-arm/arm-himix200-linux/arm-himix200-linux/include/c++/6.3.0/"
-export "CPPFLAGS=-I/home/ry10091/prefix_root/include"
-export "LDFLAGS=-L/home/ry10091/prefix_root/lib"
 
+export "PATH=/opt/hisi-linux/x86-arm/arm-himix200-linux/arm-himix200-linux/bin:${PATH}"
