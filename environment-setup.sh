9d7795b48f335f39881a8e0ae77f3f077a2066ed
Carl <ifbao@qq.com>
2019-03-06 14:28:14 +0800

    add boost

--- environment-setup.sh
+++ environment-setup.sh
@@ -1,8 +1,10 @@
 
 
+export "CXXFLAGS=-I/home/ry10091/prefix_root_arm_himix200/include"
 export "CPPFLAGS=-I/home/ry10091/prefix_root_arm_himix200/include"
-export "LDFLAGS=-L/home/ry10091/prefix_root_arm_himix200/lib"
-
+export "LDFLAGS=-L/home/ry10091/prefix_root_arm_himix200/lib -Wl,-rpath,/home/test/"
+export "PKG_CONFIG_PATH=/home/ry10091/prefix_root_arm_himix200/lib/pkgconfig"
+export "CMAKE_PREFIX_PATH=/home/ry10091/prefix_root_arm_himix200"
 
 export "CC=arm-himix200-linux-gcc"
 export "CXX=arm-himix200-linux-g++"
